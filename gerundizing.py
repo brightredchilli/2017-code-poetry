c8879b0449ea4a981854094dec7f608765f3ce49
Ying Quan Tan <ying.quan.tan@gmail.com>
2017-10-23 20:32:00 -0400

    made changes to gerundizer

--- gerundizing.py
+++ gerundizing.py
@@ -1,13 +1,12 @@
 from re import search as s
-import sys
-w=sys.argv[1]
-try:
-    if s('[bcdghjklmnopqsvxyz]$', w):
-        w+="ing"
-    elif s('[frut]$', w):
-        w+=w[-1]+"ing"
-    elif s('[aie]$', w):
-        w=w[0:-1]+"ing"
-except:
-    pass
-print w
+def gerundize(w):
+    try:
+        if s('[bcdghjklmnopqsvxyz]$', w):
+            w+="ing"
+        elif s('[frut]$', w):
+            w+=w[-1]+"ing"
+        elif s('[aie]$', w):
+            w=w[0:-1]+"ing"
+    except:
+        pass
+    return w
